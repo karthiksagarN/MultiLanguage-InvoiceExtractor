ea8c87b5d56d0bcd61d47d3998018d859be733bc
karthiksagarN <karthik.sagarn@gmail.com>
2024-06-12 21:10:03 +0530

    commit

---
+++ app.py
@@ -0,0 +1,8 @@
+### MULTI-LANGUAGE INVOICE EXTRACTOR USING GEMINI-PRO
+
+from dotenv import load_dotenv
+load_dotenv() # load the environment variables
+
+import streamlit as st
+import os
+from PIL import Image
